dd3739278e406952fdcb89a3e6be65726f688b06
Jordi <jordi.bravo.bad@gmail.com>
2021-02-11 16:58:07 -0300

    Adding button (unusable)

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,21 +2,30 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final fontStyle = new TextStyle(fontSize: 32);
+  final counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Super Tap Counter"),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 235, 27, 35),
+      appBar: AppBar(
+        title: Text('Super Tap Counter'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 235, 27, 35),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Number of taps:', style: fontStyle),
+            Text('0', style: fontStyle),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text("Number of taps:"),
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
+      ),
+    );
   }
 }
